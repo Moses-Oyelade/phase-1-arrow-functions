c3587b67e5bf2be480f59842ca1a9e3fbcf6b757
Moses <moses.oyelade@student.moringaschool.com>
2023-08-03 23:45:35 +0100

    completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+const a = 2000;
+const b = 100;
+
+
+const divide = function(a, b){
+       a / b;
+    return 20
+}
+
+divide();
+
+
+const square = (a) => a*a;
+square(2);
+
+
+let x = 3
+let y = 4 
+
+const add = (x, y) =>  x + y;
+
+add();
